fdfe774de674067c542d02ce438a5fbce10e3b35
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2024-08-10 10:27:01 -0400

    Update Autori.py

--- Autori/Autori.py
+++ Autori/Autori.py
@@ -1,16 +1,7 @@
 #/usr/bin/env/python3
-
-import sys
-
 def main():
-    input= str(sys.stdin.readline())
-
-    print(input[0], end= "")
-
-
-    for i in range(len(input)):
-        if input[i] == "-":
-            print(input[i+1], end= "")
+    for i in input().split("-"):
+        print(i[0], sep="", end ="")
 
 if __name__ == "__main__":
     main()

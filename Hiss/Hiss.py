7e76176e69ab514f929a885bdd4f0da1718a2098
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2024-08-11 12:10:48 -0400

    Update Hiss.py

--- Hiss/Hiss.py
+++ Hiss/Hiss.py
@@ -1,20 +1,9 @@
 #/usr/bin/env python3
-
-import sys
-
 def main():
-    ip= str(sys.stdin.readline())
-
-    boolean= False
-
-    for i in range(len(ip)):
-        if(ip[i]== 's' and ip[i+1]== 's'):
-            boolean= True
-
-    if(boolean== True):
-        print('hiss')
+    if "ss" in input():
+        print("hiss")
     else:
-        print('no hiss')
+        print("no hiss")
 
 if __name__ == "__main__":
     main()

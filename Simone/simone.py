5be70272a15ace39c8dae7ddeee51f9aed5451de
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2024-09-02 20:52:41 -0400

    Create simone.py

---
+++ Simone/simone.py
@@ -0,0 +1,27 @@
+def main():
+    d = {}
+    ip = input().split()
+    possible = ip[1]
+
+    for i in range(1,int(possible)+1): #start all value in the map at 0, and increment later
+        d[str(i)] = 0
+
+    colors = input().split()
+
+    for i in colors: #for each color from the input that has a corresponding key, increment the value
+        d[i] +=1 
+
+    vals = []
+    
+    for k, v in d.items():
+        if v == min(d.values()): #retrieve all keys where the value is equal to the number(s) that occurs the least often
+            vals.append(int(k))
+    
+    if len(vals) > 0: #if we have stuff to print out, print it
+        print(len(vals))
+        for i in vals:
+            print("{} ".format(i), sep = "", end = "")
+        
+
+if __name__ == "__main__":
+    main()

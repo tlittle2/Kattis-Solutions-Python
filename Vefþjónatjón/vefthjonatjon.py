e6e6ff1c7ebbd10aed1d8784b33f06f282697230
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2024-09-07 18:29:00 -0400

    Update vefthjonatjon.py

--- Vefþjónatjón/vefthjonatjon.py
+++ Vefþjónatjón/vefthjonatjon.py
@@ -1,30 +1,16 @@
-"""
-Is there a better solution than this?
-"""
 def main():
-    lst = []
-
-    c = 0
-    m = 0
-    h = 0
+    lst = [0,0,0]
 
     for _ in range(int(input())):
         ip = input().split()
         if ip[0] == 'J':
-            c+=1
+            lst[0]+=1
         if ip[1] == 'J':
-            m+=1
+            lst[1]+=1
         if ip[2] == 'J':
-            h+=1 
-    min = 10000
-    if(c < min):
-        min = c
-    if(m < min):
-        min = m
-    if (h < min):
-        min = h
-    
-    print(min)
+            lst[2]+=1
+            
+    print(min(lst))
 
 if __name__ == "__main__":
     main()

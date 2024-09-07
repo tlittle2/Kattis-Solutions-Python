742c7fa6b43959d9ab25fcc73116aeb25d00be73
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2024-09-07 17:34:11 -0400

    Create stafur.py

---
+++ Stafur/stafur.py
@@ -0,0 +1,16 @@
+def main():
+    vowels = ('A','E' 'I','O','U')
+
+    ip = (input())
+
+    if ip in vowels:
+        print("Jebb")
+    else:
+        if ip == 'Y':
+            print("Kannski")
+        else:
+            print("Neibb")
+
+if __name__ == "__main__":
+    main()
+Stafur

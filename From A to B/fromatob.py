474a5d2665540f000add5a669bec0bfce11614f8
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2024-09-08 13:04:39 -0400

    Update fromatob.py

--- From A to B/fromatob.py
+++ From A to B/fromatob.py
@@ -1,25 +1,18 @@
-"""
-time limit exceeded
-"""
-
 def main():
     a,b = map(int, input().split())
-
     moves = 0
+
     while a!=b:
-        if a <b:
-            a+=1
-            moves+=1
-        
+        if a < b: #if a is less than b, we must increase our moves and our 'a' by the range between b-a
+            moves+=b-a
+            a+=b-a
         elif a % 2!=0:
+            moves+=1
             a+=1
-            moves+=1
         else:
-            a = int(a/2)
             moves+=1
+            a = int(a/2)
+
     print(moves)
-                
-
-
 if __name__ == "__main__":
     main()

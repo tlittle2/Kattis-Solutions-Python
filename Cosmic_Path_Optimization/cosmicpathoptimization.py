b8a6ffbec0321fbf3bf06e7d8b32d6129917908c
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2024-09-13 22:43:14 -0400

    Update cosmicpathoptimization.py

--- Cosmic_Path_Optimization/cosmicpathoptimization.py
+++ Cosmic_Path_Optimization/cosmicpathoptimization.py
@@ -1,11 +1,8 @@
 def main():
-    cases = int(input())
+    int(input())
     l = list(map(int, input().split()))
     
     print(int(sum(i for i in l)/len(l)))
-
-    
-
 
 if __name__ == "__main__":
     main() 

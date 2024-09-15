44de593ed600c2d18f209bdd58581ef6b7a0a90b
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2024-09-15 12:51:19 -0400

    Create pallatolur.py

---
+++ Pallatölur/pallatolur.py
@@ -0,0 +1,12 @@
+def main(): 
+    a = int(input())
+    b = int(input())
+
+    if a<=2<=b:
+        print(1)
+        print(2)
+    else:
+        print(":(")
+        
+if __name__ == "__main__":
+    main()

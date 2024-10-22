0ec4c569c01fc8d325c28ae41f5757a87aea60f2
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2024-10-22 17:11:31 -0400

    Update Spavanac.py

--- Spavanac/Spavanac.py
+++ Spavanac/Spavanac.py
@@ -1,18 +1,12 @@
 #/usr/bin/env python3
-
 import sys
-
 def main():
     while True:
         hour, minute= map(int, sys.stdin.readline().split())
-
-
         newMinute= minute - 45
-
-
+        
         if newMinute < 0:
-            hour-=1
-            
+            hour-=1           
         print(hour%24, newMinute%60)
 
 if __name__ == "__main__":

ffe76da33fa961a0432bced55ba6dd93ce916b3f
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2024-10-27 16:16:58 -0400

    Update mumblerap.py

--- Mumble Rap/mumblerap.py
+++ Mumble Rap/mumblerap.py
@@ -3,9 +3,8 @@ import re
 #Partially Accepted
 def main():
     int(input())
-    ip = input()
-    numbers = re.findall(r"\d+", ip)
+    numbers = re.findall(r"\d+", input())
     print(max(numbers))
-                        
+
 if __name__ == "__main__":
     main()

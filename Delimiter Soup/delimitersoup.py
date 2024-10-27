00e50517ff2c447bffc9ac641b8e6410cc10e9ba
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2024-10-27 17:35:25 -0400

    Update delimitersoup.py

--- Delimiter Soup/delimitersoup.py
+++ Delimiter Soup/delimitersoup.py
@@ -7,22 +7,24 @@ def main():
 
     int(input())
     ip = input()
-    stk = [""] #initialize stack with dummy value to always make sure we have something to compare against
+    stk = []
     val = set()
 
     for i in range(len(ip)):
         if ip[i] in d.values(): #if current char is an opening delimiter, push it to the stack
             stk.append(ip[i])
-        elif ip[i] in d.keys() and d[ip[i]] == stk[-1]: #if current char is a closing delimiter and it's matching opening delimiter is at the top of the stack
-            stk.pop() #pop (what would be the opening delimiter) off the stack
-            
-            if len(stk) == 0: #append dummy value if the stack ever gets empty to ensure we have something to compare against
-                stk.append("")
-        else:
-            if ip[i] != " ":
+        
+        elif ip[i] in d.keys(): #if current char is a closing delimiter
+            if stk and d[ip[i]] == stk[-1]: # and it's matching opening delimiter is at the top of the stack
+                stk.pop() #pop opening delimiter off the stack
+            else:
                 val.add("{} {}".format(ip[i], i))
         
-    if len(val) >= 1:
+        elif ip[i] != " ":
+            val.add("{} {}".format(ip[i], i))
+            
+
+    if val:
         print("".join(val))
     else:
         print("ok so far")

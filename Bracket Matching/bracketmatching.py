cb09879fc0a05067f73a9fadbb86eb02b8bc7d5f
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2024-11-03 11:51:48 -0500

    Update bracketmatching.py

--- Bracket Matching/bracketmatching.py
+++ Bracket Matching/bracketmatching.py
@@ -1,4 +1,3 @@
-#fails at one of the end inputs
 def main():
     d = {
         "]":"[",
@@ -18,6 +17,8 @@ def main():
         elif ip[i] in d.keys(): #if current char is a closing delimiter
             if stk and d[ip[i]] == stk[-1]: # and it's matching opening delimiter is currently at the top of the stack
                 stk.pop() #pop opening delimiter off the stack
+            else:
+                stk.append(ip[i]) #otherwise, add it to the stack so that we don't forget that we had a closing delimiter with no open delimiter
     
     print("Invalid" if stk else "Valid") # if the stack has anything left after going through the input string, then it's not valid
                         

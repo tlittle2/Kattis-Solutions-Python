c5497d9e63063821088dad93c5f0a9398fb085fe
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2024-12-06 21:28:48 -0500

    Update ginfizz.py

--- Gin Fizz/ginfizz.py
+++ Gin Fizz/ginfizz.py
@@ -3,17 +3,18 @@ def main():
         "ml gin": 45,
         "ml fresh lemon juice": 30,
         "ml simple syrup": 10,
-        "slice of lemon": 1
     }
 
     ginfizz = int(input())
     
-    if ginfizz > 1: #make a copy of the key to be plural and remove the old key
-        d["slices of lemon"] = d["slice of lemon"]
-        del d["slice of lemon"]
-
     for k,v in d.items():
         print("{} {}".format(v * ginfizz, k))
+    
+    if ginfizz == 1:
+        print("{} slice of lemon".format(ginfizz))
+    else:
+        print("{} slices of lemon".format(ginfizz))
+
 
 
 if __name__ == "__main__":

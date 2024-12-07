1e4ab051868d4ed6fa57185f17088b24eca95eb9
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2024-12-07 11:13:08 -0500

    Update frumtolur.py

--- Frumtölur/frumtolur.py
+++ Frumtölur/frumtolur.py
@@ -1,17 +1,18 @@
 def isPrime(n):
     if n == 0 or n == 1:
-        return
+        return False
     elif n > 1:
         for i in range(2, n):
             if (n % i) == 0:
-                return 
-    return n
+                return False
+    return True
 
 def main():
-    primes = list(filter(lambda x: x is not None, [isPrime(i) for i in range(2, 101)]))
-
+    primes = [i for i in range(2, 101) if isPrime(i)]
+    
     for i in range(5):
         print(primes[i])
-      
+
+
 if __name__ == "__main__":
     main()

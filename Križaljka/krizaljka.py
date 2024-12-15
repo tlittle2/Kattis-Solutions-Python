1542339fdf69a43a3a77f15040e0e36e8f3bc9ef
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2024-12-15 14:24:42 -0500

    Update krizaljka.py

--- Križaljka/krizaljka.py
+++ Križaljka/krizaljka.py
@@ -1,7 +1,7 @@
 #partially right
 
 def getEarliest(a,b): #get earliest from both words where we have a matching character
-    return next((i for i, ch  in enumerate(a) if ch in b),None)
+    return next((i for i, ch  in enumerate(a) if ch in b),0)
 
 def main():
     a,b = map(list,input().split())
@@ -12,7 +12,7 @@ def main():
     grid = [["." for _ in range(len(a))] for _ in range(len(b))]
     
     grid[bIndex] = a
-
+    
     for i in range(len(b)):
         grid[i][aIndex] = b[i]
             

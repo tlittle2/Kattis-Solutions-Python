857dce06d7208421d6b626ad49ff34461fa1a995
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2024-12-15 14:20:19 -0500

    Update krizaljka.py

--- Križaljka/krizaljka.py
+++ Križaljka/krizaljka.py
@@ -1,26 +1,23 @@
 #partially right
 
-def getEarliest(a,b): #get earliest index from both words where we have a matching character
+def getEarliest(a,b): #get earliest from both words where we have a matching character
     return next((i for i, ch  in enumerate(a) if ch in b),None)
 
 def main():
-    a,b = input().split()
+    a,b = map(list,input().split())
 
     aIndex = getEarliest(a,b)
     bIndex = getEarliest(b,a)
 
     grid = [["." for _ in range(len(a))] for _ in range(len(b))]
     
-    grid[bIndex] = list(a) #place first word at correct location (replacing all "."'s with the letters of the word)
+    grid[bIndex] = a
 
-    idx = 0
-    for i in grid: #for each letter of the second word, place each letter vertically at the correct location where 2 words cross
-        i[aIndex] = list(b)[idx]
-        idx+=1
-        
-    for i in grid: #print the letters of the grid after the fact
+    for i in range(len(b)):
+        grid[i][aIndex] = b[i]
+            
+    for i in grid:
         print("".join(i))
-
 
 if __name__ == "__main__": 
     main()

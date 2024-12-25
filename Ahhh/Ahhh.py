5de0849bc82508586eaee321698cc6dcc5edcb62
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2024-12-25 14:30:00 -0500

    Update Ahhh.py

--- Ahhh/Ahhh.py
+++ Ahhh/Ahhh.py
@@ -1,17 +1,5 @@
-#/usr/bin/env python3
-
-import sys
-
-def main():
-    mAhh= str(sys.stdin.readline())
-
-    dAhh= str(sys.stdin.readline())
-
-
-    if(len(mAhh) >= len(dAhh)):
-        print('go')
-    else:
-        print('no')
+def main():   
+    print('go' if len(input()) >= len(input()) else 'no')
 
 if __name__ == "__main__":
     main()

98f43bf8b638856d6035d3b1876576b39b47e469
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2024-12-25 13:25:31 -0500

    Update maeting.py

--- Mæting/maeting.py
+++ Mæting/maeting.py
@@ -4,16 +4,13 @@ def main():
     ip1 = list(map(int, input().split()))
     ip2 = list(map(int, input().split()))
 
-    seen = set()
-
-    ordered_unique = []
+    seen = []
 
     for i in ip1:
         if i not in seen and i in ip2:
-            seen.add(i)
-            ordered_unique.append(i)
+            seen.append(i)
     
-    for i in ordered_unique:
+    for i in seen:
         print(i, sep = " ", end =" ")
 
 if __name__ == "__main__":

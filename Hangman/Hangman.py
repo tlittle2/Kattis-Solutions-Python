ded51a46b6e6f89e7609c68d89ef5e1114df4aba
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2024-12-28 22:20:05 -0500

    Update Hangman.py

--- Hangman/Hangman.py
+++ Hangman/Hangman.py
@@ -1,26 +1,13 @@
-#/usr/bin/env python3
-#this implementation does things in the order in which the letters are given in the second input
-import sys
-
 def remove_values_from_list(the_list, val):
    return [value for value in the_list if value != val]
 
-def main():
-    wordList= []
-    letterList = []
+def main():    
+    word= input().strip()
+    letters = input().strip()
 
-
+    wordList = [i for i in word]
+    letterList = [i for i in letters]
     strikes = 0
-    found = 0
-
-    word= sys.stdin.readline().strip()
-    letters = sys.stdin.readline().strip()
-
-    for i in word:
-        wordList.append(i)
-    for i in letters:
-        letterList.append(i)
-
 
     for c in letterList:
         if wordList.count(c):
@@ -28,11 +15,11 @@ def main():
             if len(wordList) == 0:
                 print("WIN")
                 break
-
         else:
             strikes += 1
             if strikes == 10:
                 print("LOSE")
                 break
+
 if __name__ == "__main__":
     main()

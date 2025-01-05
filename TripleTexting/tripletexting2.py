f53023ac699b75f4ddf790ee3b609662dd0a89e4
tlittle2 <42818211+tlittle2@users.noreply.github.com>
2025-01-05 17:56:27 -0500

    Update tripletexting2.py

--- TripleTexting/tripletexting2.py
+++ TripleTexting/tripletexting2.py
@@ -4,12 +4,12 @@ def main():
     l = []
 
     t = 0
-    while t < len(ip):
+    while t < len(ip): #process input
         l.append(ip[t:t+length])
         t+=length
 
-    d = {i : l.count(i) for i in l}
-    print(''.join(k for k,v in d.items() if v > 1))
+    d = {i : l.count(i) for i in l} #generate occurrence map
+    print(''.join(k for k,v in d.items() if v > 1)) #print the word(s) that occur more than 1 time
 
 if __name__ == "__main__":
     main()
